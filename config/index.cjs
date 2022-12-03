802953141ca6e910b6ac28e85e64266ca2bd641a
xiaowang365 <67275564+xiaowang365@users.noreply.github.com>
2022-12-03 22:50:27 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,7 +49,8 @@ const USER_CONFIG = {
         // 在一起的日子
         { keyword: 'love_day', date: '2022-09-08' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'count', date: '2022-09-09' },
+        { keyword: 'top', date: '6' },
       ],
     },
   ],
